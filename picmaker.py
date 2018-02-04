fc7452092f8e26e93997c353e737297b945e86b2
mcheng3 <michael.cheng65@gmail.com>
2018-02-04 16:51:51 -0500

    Finish picmaker.py

--- picmaker.py
+++ picmaker.py
@@ -1,1 +1,8 @@
+import math, random
+
 f = open("foo.ppm", "w")
+f.write("P3 500 500 255\n")
+i = 0
+while(i < 500 * 500):
+	f.write(str(math.sin(i + 1) % 255) +" " + str((i + random.randint(-30, 30)) % 255) + " " + str(math.tan(255 - i + 1) % 255) + " ")
+	i += 1
