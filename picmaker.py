23434c0022c091daaf2e856673c75732c35d01cd
mcheng3 <michael.cheng65@gmail.com>
2018-02-04 21:13:35 -0500

    Fix picmaker.py

--- picmaker.py
+++ picmaker.py
@@ -4,5 +4,5 @@ f = open("foo.ppm", "w")
 f.write("P3 500 500 255\n")
 i = 0
 while(i < 500 * 500):
-	f.write(str(math.sin(i + 1) % 255) +" " + str((i + random.randint(-30, 30)) % 255) + " " + str(math.tan(255 - i + 1) % 255) + " ")
+	f.write(str(int(math.sin(i + 1) % 255)) +" " + str(int(i + random.randint(-30, 30) % 255)) + " " + str(int(math.tan(255 - i + 1) % 255)) + " ")
 	i += 1
